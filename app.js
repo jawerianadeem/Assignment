d8c902e4000027b83d7cf1d0af63dee7f77b0ac8
jawerianadeem <143826078+jawerianadeem@users.noreply.github.com>
2023-09-08 02:18:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,119 @@
+// Chapter 1 (Alerts)
+// Q1
+alert("Jaweria");
+// Q2
+alert("Nadeem");
+// Q3
+alert("jawerianadeem@gmail.com");
+// Q4
+alert("03267882902");
+// Q5
+alert("password");
+// Q6
+alert("you are learning javascript");
+// Q7
+alert("Hey! Hope you all are well");
+
+
+// Chapter 2 (Variables for string)
+// Q1
+var camelCase;
+// Q2
+var num;
+num ="number1";
+// Q3
+var teamName="ayesha,alia,fiza";
+alert(teamName);
+// Q4
+var bestMan = "Fathers";
+alert(bestMan);
+
+
+// Chapter 3 (Variables for numbers)
+// Q1
+var caseQty;
+// Q2
+caseQty=144;
+// Q3
+var num="9";
+// Q4
+var sum;
+sum=3+4;
+console.log(sum);
+// Q5
+var merchTotal=100;
+var shippingCharge=10;
+var orderTotal=merchTotal+shippingCharge;
+console.log(orderTotal);
+// Q6
+var num=2;
+num + 2
+
+
+
+
+// chapter 4 (Variable names Legal and Illegal)
+// Q1
+var productCost=3.45;
+console.log(productCost)
+// Q2
+var nameOfband;
+console.log(nameOfband)
+// Q3
+var topScore = 100;
+console.log(topScore)
+// Q4
+var firstName="Jaweria";
+var lastName="Nadeem";
+console.log(firstName +" " +lastName)
+
+
+// Q5
+document.write(" legal variable var num=jiya var num54=jiya var num789one=jiya");
+document.write("illegal variable var 1num=jiya  var @num=jiya");
+
+
+
+// chapter 5 (Math Expression I)
+// Q1
+document.write(" <br> The modulo operation (abbreviated “mod”, or “%” in many programming languages) is the remainder when dividing. For example, “5 mod 3 = 2” which means 2 is the remainder when you divide 5 by 3.<br>")
+// Q2
+var num=20%6; 
+console.log(num);
+// Q3
+var largeNum = 1000 * 2000; 
+console.log(largeNum);
+// Q4
+var num1=3;
+var num2=5;
+var res=(num1 - num2);
+
+// Q5
+
+
+
+
+
+// Q6
+var num=4*2;
+alert("8");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
